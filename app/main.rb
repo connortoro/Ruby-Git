e733543cda1628047e8a9794b311f328ca51769d
connor <connor.toro@gmail.com>
2024-03-27 16:23:27 -0700

    cat-file 1

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,5 @@
+require "zlib"
+
 command = ARGV[0]
 case command
 
@@ -7,6 +9,22 @@ when "init"
   Dir.mkdir(".git/refs")
   File.write(".git/HEAD", "ref: refs/heads/main\n")
   puts "Initialized git directory"
+when "cat-file"
+  if ARGV[1] == "-p"
+    blob_sha = ARGV[2]
+    blob_path = ".git/objects/#{blob_sha[0..1]}/#{blob_sha[2..]}"
+
+    compressed_contents = File.read(blob_path, mode:"rb")
+    inflater = Zlib::Inflate.new
+    raw_contents = inflater.inflate(compressed_contents)
+    inflater.finish
+
+    idx = raw_contents.index("\0") + 1
+    print raw_contents[idx..]
+
+  else
+    raise "i dont know that one"
+  end
 else
   raise RuntimeError.new("Unknown command #{command}")
 end
