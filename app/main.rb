f93f0a362d53974614f539a29926d4eb6bb1298a
connor <connor.toro@gmail.com>
2024-03-27 18:44:03 -0700

    hash-object

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,5 @@
 require "zlib"
+require "digest"
 
 command = ARGV[0]
 case command
@@ -9,6 +10,7 @@ when "init"
   Dir.mkdir(".git/refs")
   File.write(".git/HEAD", "ref: refs/heads/main\n")
   puts "Initialized git directory"
+
 when "cat-file"
   if ARGV[1] == "-p"
     blob_sha = ARGV[2]
@@ -25,6 +27,22 @@ when "cat-file"
   else
     raise "i dont know that one"
   end
+
+when "hash-object"
+  if ARGV[1] == "-w"
+    file_contents = File.read(ARGV[2])
+    content = "blob #{file_contents.bytesize}\0#{file_contents}"
+
+    blob_sha = Digest::SHA1.hexdigest(content)
+    compressed_contents = Zlib::Deflate.deflate(content)
+
+    Dir.mkdir(".git/objects/#{blob_sha[0..1]}")
+    File.write(".git/objects/#{blob_sha[0..1]}/#{blob_sha[2..]}", "#{compressed_contents}")
+    print(blob_sha)
+  else
+    raise "i dont know that one!"
+  end
+
 else
   raise RuntimeError.new("Unknown command #{command}")
 end
